ae896426463db836c930cfd812ece73b2d8be8f1
Paul Brzeski <mail@paulbrzeski.com>
2013-04-16 23:05:22 +0800

    Bound sliders and textboxes to editor

--- client/scripts/editor/transform.js
+++ client/scripts/editor/transform.js
@@ -55,36 +55,38 @@ transform.prototype.makeControls = function () {
 
 transform.prototype.loadProperties = function(id) {
 	// clears and populates the transform window
-	var object;
+	var object, obj_index;
 	for (var i in scene.__objects) {
 		if (scene.__objects[i].id == id) {
 			object = scene.__objects[i];
+			obj_index = i;
 		}
 	};
-	$("#transform .controls .position.x.textbox").val(object.position.x);
-	$("#transform .controls .position.y.textbox").val(object.position.y);
-	$("#transform .controls .position.z.textbox").val(object.position.z);
+	$("#transform .controls .position.x.textbox").val(object.position.x).live('input',function(){ scene.__objects[obj_index].position.x =  $("#transform .controls .position.x.textbox").val(); } );
+	$("#transform .controls .position.y.textbox").val(object.position.y).live('input',function(){ scene.__objects[obj_index].position.y =  $("#transform .controls .position.y.textbox").val(); } );
+	$("#transform .controls .position.z.textbox").val(object.position.z).live('input',function(){ scene.__objects[obj_index].position.z =  $("#transform .controls .position.z.textbox").val(); } );
 	
-	$("#transform .controls .position.x.slider-control").slider( "option", "value", object.position.x );
-	$("#transform .controls .position.y.slider-control").slider( "option", "value", object.position.y );
-	$("#transform .controls .position.z.slider-control").slider( "option", "value", object.position.z );
+	$("#transform .controls .position.x.slider-control").slider( { value: object.position.x, slide: function( event, slider) {  scene.__objects[obj_index].position.x = slider.value; } } );
+	$("#transform .controls .position.y.slider-control").slider( { value: object.position.y, slide: function( event, slider) {  scene.__objects[obj_index].position.y = slider.value; } } );
+	$("#transform .controls .position.z.slider-control").slider( { value: object.position.z, slide: function( event, slider) {  scene.__objects[obj_index].position.z = slider.value; } } );
 	
-	$("#transform .controls .rotation.x.textbox").val(object.rotation.x);
-	$("#transform .controls .rotation.y.textbox").val(object.rotation.y);
-	$("#transform .controls .rotation.z.textbox").val(object.rotation.z);
+	$("#transform .controls .rotation.x.textbox").val(object.rotation.x).live('input',function(){ scene.__objects[obj_index].rotation.x =  $("#transform .controls .rotation.x.textbox").val(); } );
+	$("#transform .controls .rotation.y.textbox").val(object.rotation.y).live('input',function(){ scene.__objects[obj_index].rotation.y =  $("#transform .controls .rotation.y.textbox").val(); } );
+	$("#transform .controls .rotation.z.textbox").val(object.rotation.z).live('input',function(){ scene.__objects[obj_index].rotation.z =  $("#transform .controls .rotation.z.textbox").val(); } );
 	
-	$("#transform .controls .rotation.x.slider-control").slider( "option", "value", object.rotation.x );
-	$("#transform .controls .rotation.y.slider-control").slider( "option", "value", object.rotation.y );
-	$("#transform .controls .rotation.z.slider-control").slider( "option", "value", object.rotation.z );
+	$("#transform .controls .rotation.x.slider-control").slider( { value: object.rotation.x, slide: function( event, slider) {  scene.__objects[obj_index].rotation.x = slider.value; } } );
+	$("#transform .controls .rotation.y.slider-control").slider( { value: object.rotation.y, slide: function( event, slider) {  scene.__objects[obj_index].rotation.y = slider.value; } } );
+	$("#transform .controls .rotation.z.slider-control").slider( { value: object.rotation.z, slide: function( event, slider) {  scene.__objects[obj_index].rotation.z = slider.value; } } );
 	
-	$("#transform .controls .scale.textbox").val(object.scale.x);
+	$("#transform .controls .scale.textbox").val(object.scale.x).live('input',function(){ 
+			scene.__objects[obj_index].scale.x = $("#transform .controls .scale.textbox").val();
+			scene.__objects[obj_index].scale.y = $("#transform .controls .scale.textbox").val();
+			scene.__objects[obj_index].scale.z = $("#transform .controls .scale.textbox").val();
+	} );
 	
-	$("#transform .controls .scale.slider-control").slider( "option", "value", object.scale.x );
+	$("#transform .controls .scale.slider-control").slider( { value: object.scale.x, slide: function( event, slider) {  scene.__objects[obj_index].scale.x = slider.value; scene.__objects[obj_index].scale.y = slider.value; scene.__objects[obj_index].scale.z = slider.value; } } );
 };
 
-transform.prototype.setProperty = function(property, value) {
-	property = value;
-};
 
 transform.prototype.refresh = function() {
 	var scale = M / 8;
