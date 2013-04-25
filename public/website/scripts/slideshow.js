8b7bc3d1b28d2aff549d6a01eacdb29aa15a3e30
Paul Brzeski <mail@paulbrzeski.com>
2013-04-25 21:55:13 +0800

    Testing youtube video in slideshow

--- public/website/scripts/slideshow.js
+++ public/website/scripts/slideshow.js
@@ -9,11 +9,42 @@ $(document).ready(function () {
 		$('#slideshow ul.controls').append('<li></li>');
 		$('#slideshow ul.controls li:last-child').append('<a href="#"></a>');
 	});
-	$('#slideshow .slides').cycle({ 
-		fx:    'fade', 
+	$('#slideshow .slides').cycle({
+		fx: 'fade',
 		delay: 100,
 		pause: 1
 	});
 
+
+});
+$(window).load(function () {
+	// load Youtube Player API
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/player_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	var player;
 });
 
+function onYouTubePlayerAPIReady() {
+	$('.video').each(function () {
+		var id = $(this).attr('href').replace('http://www.youtube.com/watch?v=', '');
+		$(this).parent().append('<div id="' + id + '"></div>');
+		player = new YT.Player(id, {
+			height: '315',
+			width: '420',
+			videoId: id,
+			events: {
+				"onStateChange": pauseSlideshow()
+			}
+		});
+		$(this).remove();
+	});
+}
+function pauseSlideshow() {
+	if (this.getPlayerState > 0) {
+		$('#slideshow .slides').cycle('pause');
+		console.log('Slideshow paused');
+	}
+	
+}
